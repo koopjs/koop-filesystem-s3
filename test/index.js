5717baedf62c8595405ec030c9b5335e0e9a4154
Ben Stoltz <bstoltz@esri.com>
2016-04-11 10:07:57 -0400

    Further tests added

--- test/index.js
+++ test/index.js
@@ -7,18 +7,40 @@ const _ = require('highland')
 const zlib = require('zlib')
 const FileSystem = require('../')
 
-test('CreateWrite stream should write gzipped content correctly', (t) => {
+test('CreateWrite stream should write gzipped content correctly', t => {
   const s3fs = new FileSystem()
-  const uploadText = require('./fixtures/upload.txt')
+  
   fs.createReadStream('test/fixtures/upload.txt').pipe(s3fs.createWriteStream('upload.txt'))
 
   _(request('https://s3.amazonaws.com/koop-dev-bstoltz/upload.txt'))
     .through(zlib.createGunzip())
     .toArray(arr => {
       const txt = arr.join('').toString()
-
       t.equal(txt, '"Bob is my name"\n',
         'Uploaded and downloaded file should be equal')
     })
+  t.end()
+})
+
+test('createReadStream reads gzipped content', t => {
+  const s3fs = new FileSystem()
+
+  fs.createReadStream('test/fixtures/readStream.txt').pipe(s3fs.createWriteStream('readStream.txt'))
+  s3fs.createReadStream('readStream.txt').toArray(arr => {
+    const txt = arr.toString()
+    t.equal( txt, '"Bob is my name"\n',
+      'createReadStream handles gzipped files')
+  })
+  t.end()
+})
+
+test ('createReadStream reads non gzipped content', t => {
+  const s3fs = new FileSystem()
+
+  s3fs.createReadStream('readStreamNotGzipped.txt').toArray(arr => {
+    const txt = arr.toString()
+    t.equal(txt, '"Bob is my name"\n',
+      'createReadStream handles non gzipped files')
+  })
   t.end()
 })
