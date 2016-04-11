ab895b42a9fb5eb5b329e006526f48b933745133
Ben Stoltz <bstoltz@esri.com>
2016-04-11 10:30:11 -0400

    Fixed up t.end()

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ const FileSystem = require('../')
 
 test('CreateWrite stream should write gzipped content correctly', t => {
   const s3fs = new FileSystem()
-  
+
   fs.createReadStream('test/fixtures/upload.txt').pipe(s3fs.createWriteStream('upload.txt'))
 
   _(request('https://s3.amazonaws.com/koop-dev-bstoltz/upload.txt'))
@@ -18,8 +18,8 @@ test('CreateWrite stream should write gzipped content correctly', t => {
       const txt = arr.join('').toString()
       t.equal(txt, '"Bob is my name"\n',
         'Uploaded and downloaded file should be equal')
+      t.end()
     })
-  t.end()
 })
 
 test('createReadStream reads gzipped content', t => {
@@ -30,8 +30,9 @@ test('createReadStream reads gzipped content', t => {
     const txt = arr.toString()
     t.equal( txt, '"Bob is my name"\n',
       'createReadStream handles gzipped files')
+
+    t.end()
   })
-  t.end()
 })
 
 test ('createReadStream reads non gzipped content', t => {
@@ -41,6 +42,6 @@ test ('createReadStream reads non gzipped content', t => {
     const txt = arr.toString()
     t.equal(txt, '"Bob is my name"\n',
       'createReadStream handles non gzipped files')
+    t.end()
   })
-  t.end()
 })
